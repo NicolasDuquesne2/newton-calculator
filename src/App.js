6ea42720684ab4f72754ea00b86bfca8e5fed7e5
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-02-06 18:49:15 +0100

    Header and jumbotron done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import { useState } from 'react';
+import { texts } from './params/params'
 
 function App() {
+
+  const [radioValue, setRadioValue] = useState('1');
+  let title = ""
+  let describ = ""
+
+  const radioButtons = [
+    { name: 'Français', value: '1' },
+    { name: 'English', value: '2' }]
+
+  radioValue === '1'? title = texts.home.french.title: title = texts.home.english.title
+  radioValue === '1'? describ = texts.home.french.description: describ = texts.home.english.description
+
   return (
-    <div className="App container-fluid">
+    <div className="App container-fluid w-75">
       <header className="p-3 mb-3 bg-primary bg-gradient border">
-        <p className="text-white text-center fs-1">Welcome to Empyrion newton calculator</p>
+        <ButtonGroup aria-label="Language select">
+        {radioButtons.map((radio, idx) => (
+          <ToggleButton
+            key={`toggle-${idx}`}
+            id={`radio-${idx}`}
+            type="radio"
+            variant={'light'}
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+        </ButtonGroup>
+        <p className="text-white text-center fs-1">{title}</p>
       </header>
+      <div className='p-5 mb-4 bg-light rounded-3'>
+        <div className='container-fluid py-5'>
+          <p className='fs-3'>{describ}</p>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
--- src/params/params.jsx
+++ src/params/params.jsx
@@ -1,14 +1,14 @@
 export const texts = {
     home: {
         french: {
-            title: "Bienvenue au calculateur de newtons pour Empyrion",
+            title: "Bienvenue sur le calculateur de newtons pour Empyrion",
             description: `Cette page vous permettra de déterminer la force nécessaire à appliquer à votre vaisseau en newton
             selon vos critères de masse en Tonne ou Kilo-tonne, de gravité maximum à atteindre. Elle vous permettra aussi de
             savoir la gravité maximum supportable pour votre vaisseau`
         },
 
         english: {
-            title: "Welcome to Empyrion newton calculator",
+            title: "Welcome to the Empyrion newton calculator",
             description: `This page will allow you to determine the necessary force to apply to your vessel in Newtons 
             according to your criteria of mass in Tons or Kilo-tons, of maximum gravity to reach. 
             It will also allow you to know the maximum gravity bearable for your ship`
